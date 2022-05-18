d51460f97dc5296de2553252a21f36ee98cd1770
vishnu <vishnu@test.com>
2022-05-18 20:21:49 +0530

    This is commited by Vishnu

--- Framework_Demo/src/test/java/tests/ThreeTest.java
+++ Framework_Demo/src/test/java/tests/ThreeTest.java
@@ -13,7 +13,7 @@ public class ThreeTest extends Base {
 	@Test
 	public void testThree() throws IOException, InterruptedException {
 		System.out.println("Test Three");
-		
+		System.out.println("Vishnu has updated this code with this statement");
 		driver=initializeDriver();
 		driver.get("https://www.selenium.dev/");
 		Thread.sleep(3000);
