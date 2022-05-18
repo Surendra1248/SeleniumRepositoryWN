43bfedb1bb49204b3e13d469afa3766538efe882
Surendra1248 <102716227+Surendra1248@users.noreply.github.com>
2022-05-18 20:24:12 +0530

    Update TwoTest.java

--- Framework_Demo/src/test/java/tests/TwoTest.java
+++ Framework_Demo/src/test/java/tests/TwoTest.java
@@ -12,7 +12,7 @@ public class TwoTest extends Base {
 	@Test
 	public void testTwo() throws IOException, InterruptedException {
 		System.out.println("TestTwo");
-		
+		System.out.println("Changes made by Sruthi");
 		 driver=initializeDriver();
 		driver.get("https://www.google.co.in/");
 		Thread.sleep(3000);
